e17144ad2ec70bbd8b89a9a74eb7fb5e104b6919
Jockey780 <xlordhitmanx@gmail.com>
2024-03-04 09:30:27 +0700

    Show customer account detail

--- Asm02Solution_CarManagement/Pages/AdminPage/ManageUser/Details.cshtml.cs
+++ Asm02Solution_CarManagement/Pages/AdminPage/ManageUser/Details.cshtml.cs
@@ -6,37 +6,43 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BusinessObjects.Models;
+using Service;
+using System.Security.Principal;
 
 namespace Asm02Solution_CarManagement.Pages.AdminPage.ManageUser
 {
     public class DetailsModel : PageModel
     {
-        private readonly BusinessObjects.Models.CarManagementContext _context;
+        private readonly IUserService userService;
 
-        public DetailsModel(BusinessObjects.Models.CarManagementContext context)
+        public DetailsModel()
         {
-            _context = context;
+            userService = new UserService();
         }
 
       public User User { get; set; } = default!; 
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null || _context.Users == null)
+            if (HttpContext.Session.GetString("Role") == "Admin")
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var user = await _context.Users.FirstOrDefaultAsync(m => m.UserId == id);
-            if (user == null)
-            {
-                return NotFound();
+                User user = userService.GetUserByID((int)id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    User = user;
+                }
+                return Page();
             }
-            else 
-            {
-                User = user;
-            }
-            return Page();
+            return RedirectToPage("/Login");
         }
     }
 }
